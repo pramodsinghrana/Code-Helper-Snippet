f2fa3cc86acf11469fe5c7a3a2676fa76fb68e60
Pramod Singh Rana <37185532+pramodsinghrana@users.noreply.github.com>
2019-01-16 14:07:05 +0530

    Create LazyInitilizeClassPathXmlApplicationContext.java

---
+++ Spring/SpringLazy/LazyInitilizeClassPathXmlApplicationContext.java
@@ -0,0 +1,16 @@
+public class LazyInitilizeClassPathXmlApplicationContext extends ClassPathXmlApplicationContext {
+  public LazyInitilizeClassPathXmlApplicationContext(String[] configLocations, ApplicationContext parent) throws BeansException {
+		super(configLocations, parent);
+	}
+ 
+  /**
+   * @see org.springframework.context.support.AbstractXmlApplicationContext#initBeanDefinitionReader(org.springframework.beans.factory.xml.XmlBeanDefinitionReader)
+   */
+  @Override
+  protected void initBeanDefinitionReader(XmlBeanDefinitionReader reader) {
+    // TODO Auto-generated method stub
+    super.initBeanDefinitionReader(reader);
+    reader.setDocumentReaderClass(LazyBeanDefinitionDocumentReader.class);
+  }
+
+}
